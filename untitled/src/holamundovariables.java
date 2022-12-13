7467b714755bfc667e738d2b4b7861052ee1a654
javialroro <102406440+javialroro@users.noreply.github.com>
2022-12-12 18:15:45 -0600

    primera hora
    Hoy vi como hacer un hola mundo y las variables primitivas en java

---
+++ untitled/src/holamundovariables.java
@@ -0,0 +1,50 @@
+public class holamundovariables {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        /*
+          sout+tab
+         */
+        System.out.println("Que tal"); // comentario de una sola linea
+        //Variables
+
+        //Enteros
+        //variables byte de -128 a 128
+        byte entero;
+
+        entero=12;
+
+        byte entero1=12;
+
+        System.out.println("Entero byte: "+entero);
+
+        //variables short de -32768 a 32767
+
+        short enteros= 12456;
+        System.out.println("Entero short: "+enteros);
+
+        //variable int muchos
+
+        int enteroi=123456;
+        System.out.println("Entero int: "+enteroi);
+
+        //Decimales
+        //float
+        float decimal = 3.45f; //Poner f para evitar error
+        System.out.println("Float decimal: "+decimal);
+
+        double decimald = 3.54;
+        System.out.println("Double decimal: "+decimald);
+
+        //caracter
+
+        char caracter ='a';
+        System.out.println("char Caracter "+caracter);
+
+        // boolean
+
+        boolean decision= true;
+
+        System.out.println("Boolean: "+decision);
+        //39:43
+    }
+}
